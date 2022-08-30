41e01d36bdf9154efd72a7dc84007726e520ee8f
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-08-30 20:43:14 +0200

    Revert "Add basic home page"
    This reverts commit 3dc4b84acf3dc7ef0168694611336d69ba116593.

--- slr_spot_frontend/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Navbar } from './navbar/Navbar';
-export { default as Feature } from './feature/Feature';
--- slr_spot_frontend/src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './header/Header';
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as WhatSLRSpot } from './whatSLRSpot/WhatSLRSpot';
