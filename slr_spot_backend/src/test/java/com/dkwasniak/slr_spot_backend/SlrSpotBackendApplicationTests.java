ffbeab4c07c51d3219e6030fa4ac24998baf2822
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-09-05 09:35:09 +0200

    Init backend project

---
+++ slr_spot_backend/src/test/java/com/dkwasniak/slr_spot_backend/SlrSpotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkwasniak.slr_spot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlrSpotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
