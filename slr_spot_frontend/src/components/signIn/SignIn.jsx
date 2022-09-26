388bb77ac4471f7ceb0da0fe0bf5f615a2f5cf87
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-09-26 19:36:37 +0200

    Add connection with backend while logging

--- slr_spot_frontend/src/components/signIn/SignIn.jsx
+++ slr_spot_frontend/src/components/signIn/SignIn.jsx
@@ -1,20 +1,103 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate  } from 'react-router-dom';
+
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { login } from "../../actions/auth";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
 
 const SignIn = () => {
+  let navigate = useNavigate();
+
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  // const { isLoggedIn } = useSelector(state => state.auth);
+  // const { message } = useSelector(state => state.message);
+
+  // const dispatch = useDispatch();
+
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    form.current.validateAll();
+
+    login(username, password);
+    setLoading(false);
+    // if (checkBtn.current.context._errors.length === 0) {
+    //   dispatch(login(username, password))
+    //     .then(() => {
+    //       navigate("/profile");
+    //       window.location.reload();
+    //     })
+    //     .catch(() => {
+    //       setLoading(false);
+    //     });
+    // } else {
+    //   setLoading(false);
+    // }
+  };
+
+  // if (isLoggedIn) {
+  //   return <Navigate to="/profile" />;
+  // }
+
   return (
-    <form>
+    <Form onSubmit={handleLogin} ref={form}>
         <div className="form-inner">
             <h3>Sign In</h3>
             <p>New user? Create an account</p>
 
             <div className="form-group">
                 <label htmlFor='email'>Email:</label>
-                <input type='email' name='email' id='email'/>
+                <Input 
+                  type='text' 
+                  className="form-control" 
+                  name='email' 
+                  value={username} 
+                  onChange={onChangeUsername} 
+                  validations={[required]}
+                />
             </div>
 
             <div className="form-group">
                 <label htmlFor='password'>Password:</label>
-                <input type='password' name='password' id='password'/>
+                <Input 
+                  type='password' 
+                  className='form-control' 
+                  name='password' 
+                  value={password} 
+                  onChange={onChangePassword} 
+                  validations={[required]}
+                />
             </div>
 
             <p>Forgot password</p>
@@ -24,7 +107,7 @@ const SignIn = () => {
             <h2>Sign in with Google</h2>
             <h2>Sign in with Facebook</h2>
         </div>
-    </form>
+    </Form>
   )
 }
 
