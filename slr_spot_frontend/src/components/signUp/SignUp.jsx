852e05e28b352cbc8ce403e2529b07405005de02
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-10-18 16:10:21 +0200

    Improve signup page

--- slr_spot_frontend/src/components/signUp/SignUp.jsx
+++ slr_spot_frontend/src/components/signUp/SignUp.jsx
@@ -41,82 +41,94 @@ const SignUp = (props) => {
     <form onSubmit={handleSubmit(onSubmit)}>
       {!successful && (
         <div className='slrspot___signUp-container'>
-          <div className="slrspot___signUp-fields">
+          <div className="slrspot__signUp-header">
             <h1>Create Account</h1>
             {message && (
-              <div className="form-group">
-                <div className="alert alert-danger" style={{color: 'red'}}>
-                  {message}
-                </div>
+              <div style={{color: 'red'}}>
+                <p>{message}</p>
               </div>
             )}
-            <input 
-              {...register("firstName", { required: true })}
-              type='text' 
-              placeholder='First Name'
-              name='firstName' 
-            />
-            {errors.firstName && 
-              <p className="slrspot__signIn-error">This field is required</p>
-            }
-            <input
-              {...register("lastName", { required: true })}
-              type='text' 
-              placeholder='Last Name'
-              name='lastName' 
-            />
-            {errors.lastName && 
-              <p className="slrspot__signIn-error">This field is required</p>
-            }
-            <input
-              {...register("email", { 
-                required: true, 
-                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  
-              })}
-              placeholder='Email'
-              name='email'
-            />
-            {errors.email && errors.email.type === "pattern" &&  
-              <p className="slrspot__signIn-error">Invalid email</p>
-            }
-            {errors.email && errors.email.type=== "required" && 
-              <p className="slrspot__signIn-error">This field is required</p>
-            }
-            <input
-              {...register("password", { 
-                required: true,
-              })}
-              type='password' 
-              placeholder='Password'
-              name='password'   
-            />
-            {errors.password && 
-              <p className="slrspot__signIn-error">This field is required</p>
-            }
-            <input 
-              {...register("confirmPassword", { 
-                required: true, 
-                validate: (value) => {
-                  return watch('password') == value;}
-              })}
-              type='password' 
-              placeholder='Confirm Password'
-              name='confirmPassword'
-            />
-            {errors.confirmPassword && errors.confirmPassword.type === "validate" &&  
-              <p className="slrspot__signIn-error">Passwords do not match</p>
-            }
-            {errors.confirmPassword && errors.confirmPassword.type=== "required" && 
-              <p className="slrspot__signIn-error">This field is required</p>
-            }
+          </div>
 
-            <button type='submit'>Register</button>
-            { loading && (<BeatLoader color="#AE67FA" />)}
-            <div className="slrspot___signIn-haveAccount">
+          <div className="slrspot___signUp-fields">
+            <div className="slrspot__signUp-field">
+              <input 
+                {...register("firstName", { required: true })}
+                type='text' 
+                placeholder='First Name'
+                name='firstName' 
+              />
+              {errors.firstName && 
+                <p className="slrspot__signIn-error">This field is required</p>
+              }
+            </div>
+            <div className="slrspot__signUp-field">
+              <input
+                {...register("lastName", { required: true })}
+                type='text' 
+                placeholder='Last Name'
+                name='lastName' 
+              />
+              {errors.lastName && 
+                <p className="slrspot__signIn-error">This field is required</p>
+              }
+            </div>
+            <div className="slrspot__signUp-field">
+              <input
+                {...register("email", { 
+                  required: true, 
+                  pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  
+                })}
+                placeholder='Email'
+                name='email'
+              />
+              {errors.email && errors.email.type === "pattern" &&  
+                <p className="slrspot__signIn-error">Invalid email</p>
+              }
+              {errors.email && errors.email.type=== "required" && 
+                <p className="slrspot__signIn-error">This field is required</p>
+              }
+            </div>
+            <div className="slrspot__signUp-field">
+              <input
+                {...register("password", { 
+                  required: true,
+                })}
+                type='password' 
+                placeholder='Password'
+                name='password'   
+              />
+              {errors.password && 
+                <p className="slrspot__signIn-error">This field is required</p>
+              }
+            </div>
+            <div className="slrspot__signUp-field">
+              <input 
+                {...register("confirmPassword", { 
+                  required: true, 
+                  validate: (value) => {
+                    return watch('password') == value;}
+                })}
+                type='password' 
+                placeholder='Confirm Password'
+                name='confirmPassword'
+              />
+              {errors.confirmPassword && errors.confirmPassword.type === "validate" &&  
+                <p className="slrspot__signIn-error">Passwords do not match</p>
+              }
+              {errors.confirmPassword && errors.confirmPassword.type=== "required" && 
+                <p className="slrspot__signIn-error">This field is required</p>
+              }
+            </div>
+          </div>
+          <div className="slrspot___signUp-submit">
+              <button type='submit'>Register</button>
+              { loading && (<BeatLoader color="#AE67FA" />)}
+            </div>
+            <div className="slrspot___signUp-haveAccount">
               <span>Already have an account?</span>
               <a onClick={() => handleSignInClick(props) }>Sign In</a>
             </div>
-          </div>
         </div>
       )}
       {successful && (
