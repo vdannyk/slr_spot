062e3c40b28190ceff4be1e802c84cef4f9e5fdb
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-10-19 16:50:22 +0200

    Improve password recovery page

--- slr_spot_frontend/src/components/forgotPassword/ForgotPassword.jsx
+++ slr_spot_frontend/src/components/forgotPassword/ForgotPassword.jsx
@@ -1,26 +1,25 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import axiosInstance from "../../services/api";
+import { useForm } from "react-hook-form";
 import './forgotPassword.css';
 
 const ForgotPassword = (props) => {
-  const form = useRef();
-  const [email, setEmail] = useState("");
+  const {register, handleSubmit, formState: { errors }} = useForm();
+  const [isSuccessful, setIsSuccessful] = useState(false);
 
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
-
-  const handleForgotPassword = () => {
+  const onSubmit = (formData) => {
+    const email = formData.email;
     return axiosInstance.post(`/user/resetpassword`, null, { params: {
       email
     }})
     .then(function (response) {
       // handle success
+      setIsSuccessful(true);
       console.log(response);
     })
     .catch(function (response) {
       // handle error
+      setIsSuccessful(false);
       console.log(response);
     });
   };
@@ -32,17 +31,24 @@ const ForgotPassword = (props) => {
 
   return (
     <div className="slrspot___forgotPassword-container">
-      <form onSubmit={handleForgotPassword} ref={form}>
+      { !isSuccessful && (
+      <form onSubmit={handleSubmit(onSubmit)}>
           <h1>RESET PASSWORD</h1>
           <p>Enter the email address associated with your account and we'll send you a link to reset your password</p>
-          <input 
-            type='email' 
-            className='slrspot__forgotPassword-inputField' 
+          <input  
+            {...register("email", { 
+              required: true, 
+              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/  
+            })}
             placeholder='Email' 
             name='email' 
-            required
-            onChange={onChangeEmail}
           />
+          {errors.email && errors.email.type === "pattern" &&  
+            <p className="slrspot__signIn-error">Invalid email</p>
+          }
+          {errors.email && errors.email.type=== "required" && 
+            <p className="slrspot__signIn-error">This field is required</p>
+          }
 
           <button type="submit" className='slrspot__forgotPassword-submitBtn'>
             Submit
@@ -52,6 +58,16 @@ const ForgotPassword = (props) => {
             <a onClick={() => handleBackToLoginClick(props) }>Back to login</a>
           </div>
       </form>
+      )}
+      { isSuccessful && (
+        <div>
+          <h1>Reset link send</h1>
+          <p>Check your email and reset password</p>
+          <div>
+            <button onClick={() => handleBackToLoginClick(props) }>Back to login</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
