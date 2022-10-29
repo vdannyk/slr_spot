ab870d5150ae110198c239e1c956cced0b03e516
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-10-29 18:16:22 +0200

    Improve homepage

--- slr_spot_frontend/src/App.js
+++ slr_spot_frontend/src/App.js
@@ -3,14 +3,16 @@ import AppRoutes from './AppRoutes';
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { Footer } from './containers';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="slrspot__app">
       <Router>
         <Navbar />
         <AppRoutes />
+        <Footer />
       </Router>
     </div>
   );
--- slr_spot_frontend/src/containers/index.js
+++ slr_spot_frontend/src/containers/index.js
@@ -1,6 +1,6 @@
-export { default as Header } from './header/Header';
 export { default as Footer } from './footer/Footer';
 export { default as WhatSLRSpot } from './whatSLRSpot/WhatSLRSpot';
 export { default as AccessPopup } from './access/AccessPopup';
 export { default as HomePage } from './homePage/homePage';
 export { default as Reviews } from './reviews/Reviews';
+export { default as Profile } from './profile/Profile';
