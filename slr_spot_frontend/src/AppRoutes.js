7bb7e7810d4a496cc06fad65a3c47b569b499e87
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-10-30 21:38:13 +0100

    Add routes auth

--- slr_spot_frontend/src/AppRoutes.js
+++ slr_spot_frontend/src/AppRoutes.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useSelector } from "react-redux";
 import { HomePage, Profile, Reviews } from './containers';
 import { NewReview, SignInPage, AccountActivation, PasswordRecovery } from './components';
 
-export default () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="signin" element={<SignInPage />} />
-    <Route path="profile" element={<Profile />} />
-    <Route path="password-recovery/:resetToken" element={<PasswordRecovery />} />
-    <Route path="activate/:activationToken" element={<AccountActivation />} />
-    <Route path="reviews" element={<Reviews />} />
-    <Route path="reviews/new" element={<NewReview />} />
-  </Routes>
-);
+
+const AppRoutes = () => {
+  const { isLoggedIn } = useSelector(state => state.auth);
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="signin" element={<SignInPage />} />
+      <Route path="profile" element={isLoggedIn ? <Profile /> : <Navigate to='/' />} />
+      <Route path="password-recovery/:resetToken" element={<PasswordRecovery />} />
+      <Route path="activate/:activationToken" element={<AccountActivation />} />
+      <Route path="reviews" element={isLoggedIn ? <Reviews /> : <Navigate to='/' />} />
+      <Route path="reviews/new" element={isLoggedIn ? <NewReview />: <Navigate to='/' />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes;
