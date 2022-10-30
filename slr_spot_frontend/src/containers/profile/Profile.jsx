091569735f2d35ea7bf83f3773a0b2927b77eaa5
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-10-30 18:33:34 +0100

    Add forms in profile settings

--- slr_spot_frontend/src/containers/profile/Profile.jsx
+++ slr_spot_frontend/src/containers/profile/Profile.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import { CgProfile } from "react-icons/cg";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
 import './profile.css'
 
 
 const Profile = () => {
   const { user: currentUser } = useSelector((state) => state.auth);
+  const {register, handleSubmit, watch, formState: { errors }} = useForm();
+  const {
+    register: registerName, 
+    handleSubmit: handleSubmitName, 
+    watch: watchName, 
+    formState: { errors: errorsName }
+  } = useForm();
+  const {
+    register: registerPassword, 
+    handleSubmit: handleSubmitPassword, 
+    watch: watchPassword, 
+    formState: { errors: errorsPassword }
+  } = useForm();
+  
+
+  const onEmailSubmit = (formData) => {
+    console.log(formData);
+  };
+
+  const onNameSubmit = (formData) => {
+    console.log(formData);
+  };
+
+  const onPasswordSubmit = (formData) => {
+    console.log(formData);
+  };
 
   return (
     <div className='slrspot__profile'>
@@ -17,36 +44,135 @@ const Profile = () => {
           <p>Profile</p>
         </div>
         <div className='slrspot__profile-profileSettings'>
+
           <div className='slrspot__profile-baseInfo'>
             <div className='slrspot__profile-avatar'>
               <p>Your Avatar</p>
               <CgProfile size={100}></CgProfile>
               <button>Change avatar</button>
             </div>
-            <div className='slrspot__profile-name'>
-              <label>First name</label>
-              <input placeholder={currentUser.firstName}/>
-              <label>Last name</label>
-              <input placeholder={currentUser.lastName}/>
-              <button>Save</button>
-            </div>
+            
+            <form onSubmit={handleSubmitName(onNameSubmit)}>
+              <div className='slrspot__profile-name'>
+                <label>First name</label>
+                <input 
+                  {...registerName("firstName", { 
+                    required: true,
+                    validate: () => {
+                      return watchName('firstName').length <= 50;}
+                  })}
+                  type='text' 
+                  placeholder={currentUser.firstName}
+                  name='firstName' 
+                />
+                {errorsName.firstName && errorsName.firstName.type === "required" && 
+                  <p className="slrspot__profile-error">This field is required</p>
+                }
+                {errorsName.firstName && errorsName.firstName.type === "validate" &&  
+                  <p className="slrspot__profile-error">Input is too long</p>
+                }
+                <label>Last name</label>
+                <input
+                  {...registerName("lastName", { 
+                    required: true,
+                    validate: () => {
+                      return watchName('lastName').length <= 50;}
+                  })}
+                  type='text' 
+                  placeholder={currentUser.lastName}
+                  name='lastName' 
+                />
+                {errorsName.lastName && errorsName.lastName.type === "required" &&  
+                  <p className="slrspot__profile-error">This field is required</p>
+                }
+                {errorsName.lastName && errorsName.lastName.type === "validate" &&  
+                  <p className="slrspot__profile-error">Input is too long</p>
+                }
+                <button type='submit'>Save</button>
+              </div>
+            </form>
           </div>
+
           <h2>Change your email</h2>
-          <div className='slrspot__profile-email'>
-            <label>Email</label>
-            <input placeholder={currentUser.username}/>
-            <button>Save</button>
-          </div>
+          <form onSubmit={handleSubmit(onEmailSubmit)}>
+            <div className='slrspot__profile-email'>
+              <label>Email</label>
+              <input
+                {...register("email", { 
+                  required: true, 
+                  pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  validate: () => {
+                    return watch('email').length <= 65;}  
+                })}
+                placeholder={currentUser.username}
+                name='email'
+              />
+              {errors.email && errors.email.type === "pattern" &&  
+                <p className="slrspot__profile-error">Invalid email</p>
+              }
+              {errors.email && errors.email.type=== "required" && 
+                <p className="slrspot__profile-error">This field is required</p>
+              }
+              {errors.email && errors.email.type === "validate" &&  
+                <p className="slrspot__profile-error">Input is too long</p>
+              }
+              <button type="submit">Save</button>
+            </div>
+          </form>
+
           <h2>Change your password</h2>
-          <div className='slrspot__profile-password'>
-            <label>Old password</label>
-            <input/>
-            <label>New Password</label>
-            <input/>
-            <label>Confirm New Password</label>
-            <input/>
-            <button>Save</button>
-          </div>
+          <form onSubmit={handleSubmitPassword(onPasswordSubmit)}>
+            <div className='slrspot__profile-password'>
+              <label>Old password</label>
+              <input 
+                {...registerPassword("oldPassword", { 
+                  required: true,
+                })}
+                type='password' 
+                placeholder='Old Password'
+                name='oldPassword'
+              />
+              {errorsPassword.oldPassword && errorsPassword.oldPassword.type=== "required" && 
+                <p className="slrspot__profile-error">This field is required</p>
+              }
+              <label>New Password</label>
+              <input
+                {...registerPassword("newPassword", { 
+                  required: true,
+                  validate: (value) => {
+                    return watchPassword('oldPassword') != value;}
+                })}
+                type='password' 
+                placeholder='New Password'
+                name='newPassword'   
+              />
+              {errorsPassword.newPassword && errorsPassword.newPassword.type === "validate" &&  
+                <p className="slrspot__profile-error">New password cannot be the same as your current password</p>
+              }
+              {errorsPassword.newPassword && errorsPassword.newPassword.type=== "required" && 
+                <p className="slrspot__profile-error">This field is required</p>
+              }
+              <label>Confirm New Password</label>
+              <input 
+                {...registerPassword("confirmPassword", { 
+                  required: true, 
+                  validate: (value) => {
+                    return watchPassword('newPassword') == value;}
+                })}
+                type='password' 
+                placeholder='Confirm Password'
+                name='confirmPassword'
+              />
+              {errorsPassword.confirmPassword && errorsPassword.confirmPassword.type === "validate" &&  
+                <p className="slrspot__profile-error">Passwords do not match</p>
+              }
+              {errorsPassword.confirmPassword && errorsPassword.confirmPassword.type=== "required" && 
+                <p className="slrspot__profile-error">This field is required</p>
+              }
+              <button type="submit">Save</button>
+            </div>
+          </form>
+
         </div>
       </div>
     </div>
