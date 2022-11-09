2f512a04b55cb86c078e49953bda73c3b3c61987
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-11-09 21:10:45 +0100

    Add basic review page

--- slr_spot_frontend/src/containers/review/Review.jsx
+++ slr_spot_frontend/src/containers/review/Review.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../../services/api";
+import { AiFillCaretRight,  AiFillCaretLeft } from "react-icons/ai";
+import './review.css'
 
 
 const Review = () => {
   const { reviewId } = useParams();
   const navigate = useNavigate();
   const [reviewData, setReviewData] = useState([]);
+  const [showMenu, setShowMenu] = useState(true);
 
 
   useEffect(() => {
@@ -18,8 +21,28 @@ const Review = () => {
     });
   }, []);
 
+  const Menu = () => (
+    <div className='slrspot__review-menu'>
+      <div className='slrspot__review-menu-options'>
+        <p>Jedna opcja</p>
+        <p>Druga opcja</p>
+        <p>Trzecia opcja</p>
+        <p>Czwarta opcja</p>
+        <p>Piąta opcja</p>
+      </div>
+    </div>
+  )
+
   return (
-    <div>{reviewData.title}</div>
+    <div className='slrspot__review'>
+      { showMenu && <Menu /> }
+      <div className='slrspot__review-menu-show' onClick={() => setShowMenu(!showMenu)}>
+        { showMenu ? <AiFillCaretLeft /> : <AiFillCaretRight />}
+      </div>
+      <div className='slrspot__review-content'>
+        <h1>{reviewData.title}</h1>
+      </div>
+    </div>
   )
 }
 
