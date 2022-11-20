b402a127ce9439a7b63fc03f720c05b133c49c3b
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-11-20 18:48:01 +0100

    Remove unused container

--- slr_spot_frontend/src/containers/index.js
+++ slr_spot_frontend/src/containers/index.js
@@ -1,5 +1,4 @@
 export { default as Footer } from './footer/Footer';
-export { default as WhatSLRSpot } from './whatSLRSpot/WhatSLRSpot';
 export { default as AccessPopup } from './access/AccessPopup';
 export { default as HomePage } from './homePage/homePage';
 export { default as Reviews } from './reviews/Reviews';
