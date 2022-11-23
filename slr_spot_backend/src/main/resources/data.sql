9c3c8275da07bb201b20f29d00df0f805cd6cef0
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-11-23 23:04:44 +0100

    Add more test data

---
+++ slr_spot_backend/src/main/resources/data.sql
@@ -0,0 +1,128 @@
+-- Aktywne konta
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Daniel', 'Danielowski', 'danny@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Bruno', 'Wróblewski', 'bruno@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Paweł', 'Krupa', 'krupa@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Rafał', 'Nowak', 'rnowak@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Marek', 'Lis', 'mlis@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Ariel', 'Kwiatkowski', 'akwiat@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Olga', 'Piotrowska', 'opiotr@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Elwira', 'Czarnecka', 'eczarnecka@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Bogna', 'Pawlak', 'bpawlak@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Agnieszka', 'Kozłowska', 'kozel@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Nikola', 'Witkowska', 'nwitek@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Andrea', 'Nowak', 'andrea@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Andżelika', 'Woźniak', 'andzela@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Edyta', 'Górecka', 'edycia@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Amir', 'Jasiński', 'amir@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Aleksy', 'Górecki', 'alek@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Remigiusz', 'Sokołowski', 'remeksokol@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Piotr', 'Zieliński', 'zielu@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Robert', 'Lewangoalski', 'polskagola@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', true);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+-- Nieaktywne konta
+VALUES ('Martin', 'Krajewski', 'martini@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', false);
+INSERT INTO users(FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, IS_ACTIVATED)
+VALUES ('Rafał', 'Sikor', 'sikorka@gmail.com', '$2a$12$EQYycNG5NRX8ZLLjd14c.OrFcqh0hbGplRZj8QIdUrAn2383ApwLG', false);
+
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Co robią żółwie', 'Nauki przyrodnicze', 'Wszystko o żółwiach i nie tylko', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Testy jednostkowe', 'Inżynieria oprogramowania', 'To co programiści lubią robić najbardziej, czyli testy jednostkowe i wszystko co możliwe w okół nich', true, 2);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('IntellijIdea i jego skróty', 'Oprogramowanie', 'Klawisze, szybkie akcje, ułatwienie pracy każdego programisty i nie tylko', true, 2);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Co dobrego daje Postman', 'Oprogramowanie', 'Żadania wysyłane i żądania otrzymywanie to w dzisiejszych czasach codzienność', false, 2);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Leki na ból głowy', 'Medycyna', 'Każdego boli czasem głowa ale nikt nie wie czemu go boli głowa dopóki się nie dowie dlaczego go boli głowa i w tym bólu głowy wszystko tak tkwi', false, 2);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd1', 'Przykładowy przegląd1', 'Przykładowy przegląd1', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd2', 'Przykładowy przegląd2', 'Przykładowy przegląd2', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd3', 'Przykładowy przegląd3', 'Przykładowy przegląd3', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd4', 'Przykładowy przegląd4', 'Przykładowy przegląd4', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd5', 'Przykładowy przegląd5', 'Przykładowy przegląd5', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd6', 'Przykładowy przegląd6', 'Przykładowy przegląd6', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd7', 'Przykładowy przegląd7', 'Przykładowy przegląd7', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd8', 'Przykładowy przegląd8', 'Przykładowy przegląd8', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd9', 'Przykładowy przegląd9', 'Przykładowy przegląd9', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd10', 'Przykładowy przegląd10', 'Przykładowy przegląd10', true, 1);
+INSERT INTO reviews(TITLE, RESEARCH_AREA, DESCRIPTION, IS_PUBLIC, SCREENING_REVIEWERS)
+VALUES ('Przykładowy przegląd11', 'Przykładowy przegląd11', 'Przykładowy przegląd11', true, 1);
+
+-- Projekt 1
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 1, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 2, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 3, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 4, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 5, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 6, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 7, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 8, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 9, 1);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 10, 1);
+
+-- Projekt 2
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 1, 2);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 2, 2);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 3, 2);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 4, 2);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 5, 2);
+
+-- Projekt 3
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 2, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 10, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 11, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 12, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 13, 3);
+
+-- Projekt 4
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 1, 4);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 2, 4);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 13, 4);
+
+-- Projekt 5
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 3, 5);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 9, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 8, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 7, 3);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (1, 6, 3);
+
+-- Reszta
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 20, 6);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 20, 7);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 20, 8);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 20, 9);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 20, 10);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 10, 11);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 11, 12);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 12, 13);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 13, 14);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 14, 15);
+INSERT INTO users_reviews(ROLE, USER_ID, REVIEW_ID) VALUES (0, 15, 16);
