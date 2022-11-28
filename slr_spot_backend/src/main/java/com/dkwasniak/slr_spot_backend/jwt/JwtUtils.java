7baed9eedd9155221ff1eb5f934b596c977c3678
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-11-28 21:38:36 +0100

    Set jwt expiration very long

--- slr_spot_backend/src/main/java/com/dkwasniak/slr_spot_backend/jwt/JwtUtils.java
+++ slr_spot_backend/src/main/java/com/dkwasniak/slr_spot_backend/jwt/JwtUtils.java
@@ -15,8 +15,8 @@ public class JwtUtils {
 
     private final static String SECRET = "secret";
     private final static String ROLES_CLAIM = "roles";
-    private final static long JWT_EXPIRATION= 10 * 60 * 1000;
-    private final static long REFRESH_TOKEN_EXPIRATION = 20 * 60 * 1000;
+    private final static long JWT_EXPIRATION= 100 * 60 * 1000;
+    private final static long REFRESH_TOKEN_EXPIRATION = 200 * 60 * 1000;
     private static final String AUTHORIZATION_PREFIX = "Bearer ";
 
     public static DecodedJWT validateJwt(String jwt) {
