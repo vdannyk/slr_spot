5f7fba7ae79761d0658735c2aef5c1069ec326fb
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-12-10 16:11:53 +0100

    Add checkboxes in studies display

--- slr_spot_frontend/src/containers/review/studies/studiesDisplay/studiesView/StudiesView.jsx
+++ slr_spot_frontend/src/containers/review/studies/studiesDisplay/studiesView/StudiesView.jsx
@@ -1,65 +1,75 @@
 import React from 'react';
-import { AiOutlineClose } from "react-icons/ai";
+import Check from 'react-bootstrap/FormCheck';
 import { Table } from "react-bootstrap";
 
 import './studiesView.css';
+import { useState } from 'react';
 
 const testImprots = [
   {
-    "importedAt": "xx/xx/xxxx",
-    "performedBy": "tester",
-    "searchValue": "bobry",
-    "source": "Web of science",
-    "studiesAdded": 0,
-    "duplicatesRemoved": "Not found",
+    "id": 1,
+    "title": "title",
+    "authors": "author",
+    "year": "1000",
   },
   {
-    "importedAt": "xx/xx/xxxx",
-    "performedBy": "tester",
-    "searchValue": "bobry",
-    "source": "Web of science",
-    "studiesAdded": 0,
-    "duplicatesRemoved": "Not found",
+    "id": 2,
+    "title": "title2",
+    "authors": "author2",
+    "year": "1002",
   },
   {
-    "importedAt": "xx/xx/xxxx",
-    "performedBy": "tester",
-    "searchValue": "bobry",
-    "source": "Web of science",
-    "studiesAdded": 0,
-    "duplicatesRemoved": "Not found",
+    "id": 3,
+    "title": "title3",
+    "authors": "author3",
+    "year": "1003",
   },
   {
-    "importedAt": "xx/xx/xxxx",
-    "performedBy": "tester",
-    "searchValue": "bobry",
-    "source": "Web of science",
-    "studiesAdded": 0,
-    "duplicatesRemoved": "Not found",
+    "id": 4,
+    "title": "title4",
+    "authors": "author4",
+    "year": "1004",
   },
   {
-    "importedAt": "xx/xx/xxxx",
-    "performedBy": "tester",
-    "searchValue": "bobry",
-    "source": "Web of science",
-    "studiesAdded": 0,
-    "duplicatesRemoved": "Not found",
+    "id": 5,
+    "title": "title5",
+    "authors": "author5",
+    "year": "1005"
   }
 ]
 
 const StudiesView = () => {
+  const [selected, setSelected] = useState([]);
+
+  const handleSelect = (study) => {
+    if (selected.includes(study)) {
+      setSelected(selected.filter(item => item.id !== study.id));
+    } else {
+      setSelected(oldArray => [...oldArray, study]);
+    }
+  }
+
+  const handleSelectAll = (event) => {
+      if (event.target.checked) {
+        setSelected(testImprots);
+      } else {
+        setSelected([]);
+      }
+  }
 
   const listTestData = testImprots.map((item, id) => 
     <tbody key={id}>
       <tr>
+        <td>
+          <Check 
+            type='checkbox'
+            checked={ selected.includes(item) }
+            onChange={ () => handleSelect(item) } />
+        </td>
         <td>{id + 1}</td>
-        <td>{item.importedAt}</td>
-        <td>{item.performedBy}</td>
-        <td>{item.searchValue}</td>
-        <td>{item.source}</td>
-        <td>{item.studiesAdded}</td>
-        <td>{item.duplicatesRemoved}</td>
-        <td><AiOutlineClose /></td>
+        <td>{item.title}</td>
+        <td>{item.authors}</td>
+        <td>{item.year}</td>
       </tr>
     </tbody>
   );
@@ -69,6 +79,14 @@ const StudiesView = () => {
       <Table>
         <thead>
           <tr>
+            <th>
+              <div className='d-flex'>
+                <Check 
+                  type='checkbox'
+                  checked={ selected.length === testImprots.length } 
+                  onChange={ handleSelectAll }/><span>All</span>
+              </div>
+            </th>
             <th>#</th>
             <th>Title</th>
           </tr>
