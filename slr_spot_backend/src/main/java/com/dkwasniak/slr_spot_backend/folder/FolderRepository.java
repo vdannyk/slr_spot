be7e4f4716787737ec6d6d9fa2755f6336a70355
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-12-16 16:19:37 +0100

    Improve folder repository

--- slr_spot_backend/src/main/java/com/dkwasniak/slr_spot_backend/folder/FolderRepository.java
+++ slr_spot_backend/src/main/java/com/dkwasniak/slr_spot_backend/folder/FolderRepository.java
@@ -1,9 +1,11 @@
 package com.dkwasniak.slr_spot_backend.folder;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
     List<Folder> findByParentIsNull();
 }
