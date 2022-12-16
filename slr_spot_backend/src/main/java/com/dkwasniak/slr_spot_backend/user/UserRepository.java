64b3856cccb527ae38ed5b30ee5e18329b1cc677
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2022-12-16 22:04:10 +0100

    Refactor

--- slr_spot_backend/src/main/java/com/dkwasniak/slr_spot_backend/user/UserRepository.java
+++ slr_spot_backend/src/main/java/com/dkwasniak/slr_spot_backend/user/UserRepository.java
@@ -3,7 +3,6 @@ package com.dkwasniak.slr_spot_backend.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
@@ -12,7 +11,6 @@ import java.util.Set;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-    Set<User> findByReviews_Review_Id(long reviewId);
     @Transactional
     @Modifying
     @Query("UPDATE User u " +
@@ -23,16 +21,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u.email " +
             "FROM User u ")
     Set<String> getEmails();
-    @Query("SELECT u.email " +
-            "FROM User u " +
-            "WHERE u.email <> :currentEmail " +
-            "and not exists (" +
-            "    SELECT u.email " +
-            "    FROM User u" +
-            "             LEFT JOIN UserReview ur " +
-            "                       ON u.id = ur.user.id " +
-            "    WHERE ur.review.id = :currentReview" +
-            ")")
-    Set<String> getAllowedEmails(@Param("currentEmail") String currentUserEmail,
-                                 @Param("currentReview") long currentReviewId);
+
 }
