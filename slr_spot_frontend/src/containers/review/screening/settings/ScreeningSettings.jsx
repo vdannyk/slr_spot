a3d02d6a821150a04d5d626e9d5b1196544621da
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2023-01-07 01:32:50 +0100

    Improve selector in screening setttings

--- slr_spot_frontend/src/containers/review/screening/settings/ScreeningSettings.jsx
+++ slr_spot_frontend/src/containers/review/screening/settings/ScreeningSettings.jsx
@@ -9,16 +9,21 @@ const ScreeningSettings = () => {
   
   return (
     <div className='slrspot__review-screening'>
+
       <div className='slrspot__review-screening-menu'>
+
         <div className='slrspot__review-header'>
           <h1>Screening settings</h1>
         </div >
+
         <div className='slrspot__review-screening-menu-options'>
           <h2 onClick={() => navigate(location.pathname + '/criteria')}><li>Inclusion and exclusion criteria</li></h2>
           <h2 onClick={() => navigate(location.pathname + '/tags')}><li>Tags</li></h2>
           <h2 onClick={() => navigate(location.pathname + '/keywords')}><li>Keywords</li></h2>
         </div>
+        
       </div>
+
     </div>
   )
 }
