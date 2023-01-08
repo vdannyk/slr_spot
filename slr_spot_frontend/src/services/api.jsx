e2f8ba7effdd60f252e07527bb798b8202b311cf
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2023-01-08 13:43:06 +0100

    Add prod config for azure

--- slr_spot_frontend/src/services/api.jsx
+++ slr_spot_frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://primary:hfbPPW0lQQf8Z2ygnx0P4V01wlQwAkwq0l1v5GpvgWzNsw4cTM1bTq4Dx6bmIvHN@slrspot-backend.test.azuremicroservices.io/slrspot-server/default/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
