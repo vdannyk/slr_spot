adb678f89d187c8f3793b6d7803adf03d6fbcf33
Daniel <daniel.kwasniak.stud@pw.edu.pl>
2023-01-23 00:45:34 +0100

    Add emailService test

---
+++ slr_spot_backend/src/test/java/com/dkwasniak/slr_spot_backend/email/DefaultEmailServiceTest.java
@@ -0,0 +1,41 @@
+package com.dkwasniak.slr_spot_backend.email;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
+import org.thymeleaf.TemplateEngine;
+
+import javax.mail.internet.MimeMessage;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class DefaultEmailServiceTest {
+
+    @InjectMocks
+    private DefaultEmailService emailService;
+
+    @Mock
+    private JavaMailSender mailSender;
+    @Mock
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void sendEmail_shouldSendEmail_whenCorrectMessage() {
+        when(mailSender.createMimeMessage()).thenReturn(mock(MimeMessage.class));
+
+        emailService.sendEmail("me", "you", "test");
+
+        verify(mailSender, times(1)).send((MimeMessage) any());
+    }
+
+}
